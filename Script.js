1e4b9a979c9c2302225f4b042b227c79266fc1d5
lordlongbow <102767169+lordlongbow@users.noreply.github.com>
2022-05-29 06:52:49 -0300

    Adelantos en javascript
    finalizacion carrusel, formulario, banner

--- Script.js
+++ Script.js
@@ -32,6 +32,93 @@ function izquierda() {
 }
 
 function iniciar() {
-    foto= document.getElementById("carrucelpic");
-    
+    foto= document.getElementById("carrucelpic"); 
 }
+/*formulario*/
+
+console.log("COMENZAMOS");
+
+var nombre = document.getElementById("nombre");
+var telefono = document.getElementById("telefono");
+var email = document.getElementById("email");
+var radios = document.getElementsByName("radio");
+var formulario = document.getElementById("formulario");
+var array = [];
+
+function validar() {
+
+    if ( validonombre() && validotelefono() && validoemail() && validoradios()&& validoradios()) {
+        array.push({ nombre: nombre.value, telefono: telefono.value, email: email.value,  comentarios: comentarios.value });
+        //crearElementos();
+       return true;
+    }
+    
+    return false;
+}
+
+function validonombre() {
+    if (nombre.value == "") {
+        console.log("Nombre no debe estar vacio");
+        document.getElementById("nombre").style.border = "1px solid red"
+        nombre.focus();
+        return false;
+    } else {
+        document.getElementById("nombre").style.border = "1px solid black"
+        return true;
+    }
+}
+
+function validotelefono(){
+//debo validarlo por expresiones regulares preguntar el martes
+    
+    if(telefono.value== ""){
+telefono.focus();
+    telefono.style.border ="1px solid red"
+return false;
+}else{
+    return true;
+}
+}
+
+function validoemail(){
+    //debo validarlo por expresiones regulares preguntar el martes
+        
+        if(email.value== ""){
+    email.focus();
+       email.style.border ="1px solid red"
+    return false;
+    }else{
+        return true;
+    }
+    }
+
+
+function validoradios() {
+  if(radios[0].checked || radios[1].checked){
+      console.log(radios.value)
+      console.log("Y");
+      return true;
+  }else{   
+    console.log("X")
+      return false;
+  }
+
+}
+/*
+function crearDatos() {
+   contenedorcreado.innerHTML = "";
+    array?.forEach(aux => {
+        let elemento = document.createElement("div");
+        elemento.className= "creado2";
+      if (aux.seleccion == "Saluda" && aux.comentarios == "") {
+            elemento.innerHTML = "<p>" + aux.nombre + " Ha Saludado" + "</p>"
+
+        } else if (aux.seleccion == "Saluda" && aux.comentarios != "") {
+            elemento.innerHTML = "<p>" + aux.nombre + " deja un mensaje: " + aux.comentarios + "</p>"
+        } else {
+            elemento.innerHTML = "<p>" + aux.nombre + " deja una sugerencia : " + aux.comentarios + "</p>"           
+        }
+        contenedorcreado.appendChild(elemento)
+    })
+}
+*/
