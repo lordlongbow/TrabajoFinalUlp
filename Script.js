965f5c52f1deaa8f4f94abdd28b32663a8c31193
lordlongbow <102767169+lordlongbow@users.noreply.github.com>
2022-05-30 05:37:08 -0300

    cambios
    se continua intentando hacer funcionar el javascript del formulario

--- Script.js
+++ Script.js
@@ -1,10 +1,10 @@
-var imagenes=[];
+var imagenes = [];
 
-imagenes[0]= "img/Arboles (1).png"
-imagenes[1]= "img/colmena (7).jpg"
-imagenes[2]= "img/Abejas (7).png"
-imagenes[3]= "img/colmena (10).jpg"
-imagenes[4]= "img/Arboles (3).png"
+imagenes[0] = "img/Arboles (1).png"
+imagenes[1] = "img/colmena (7).jpg"
+imagenes[2] = "img/Abejas (7).png"
+imagenes[3] = "img/colmena (10).jpg"
+imagenes[4] = "img/Arboles (3).png"
 
 var foto;
 var cont = 0;
@@ -32,11 +32,11 @@ function izquierda() {
 }
 
 function iniciar() {
-    foto= document.getElementById("carrucelpic"); 
+    foto = document.getElementById("carrucelpic");
 }
 /*formulario*/
 
-console.log("COMENZAMOS");
+
 
 var nombre = document.getElementById("nombre");
 var telefono = document.getElementById("telefono");
@@ -48,20 +48,21 @@ var array = [];
 
 function validar() {
 
-    if ( validonombre() && validotelefono() && validoemail() && validoradios()&& validoradios()) {
-        array.push({ nombre: nombre.value, telefono: telefono.value, email: email.value,  comentarios: comentarios.value });
-        //crearElementos();
-       return true;
+    if (validonombre() && validotelefono() && validoemail() && validoradios() && validoradios()) {
+        array.push({ nombre: nombre.value, telefono: telefono.value, email: email.value, comentarios: comentarios.value });
+        crearElementos();
     }
-    
+
     return false;
 }
 
 function validonombre() {
     if (nombre.value == "") {
         console.log("Nombre no debe estar vacio");
-        document.getElementById("nombre").style.border = "1px solid red"
+        document.getElementById("nombre").style.border = "2px solid red"
+        nombre.placeholder = "Debes Escribir tu nombre"
         nombre.focus();
+
         return false;
     } else {
         document.getElementById("nombre").style.border = "1px solid black"
@@ -69,61 +70,68 @@ function validonombre() {
     }
 }
 
-function validotelefono(){
-//debo validarlo por expresiones regulares preguntar el martes
-    
-    if(telefono.value== ""){
-        console.log("telefono no debe estar vacio");
-telefono.focus();
-    telefono.style.border ="1px solid red"
-return false;
-}else{
-    return true;
-}
-}
-
-function validoemail(){
+function validotelefono() {
     //debo validarlo por expresiones regulares preguntar el martes
-        
-        if(email.value== ""){
-            console.log("pone el email");
-    email.focus();
-       email.style.border ="1px solid red"
-    return false;
-    }else{
+
+    if (telefono.value == "") {
+        console.log("telefono no debe estar vacio");
+        telefono.placeholder = "Debes Escribir tu telefono"
+        telefono.focus();
+        telefono.style.border = "2px solid red"
+        return false;
+    } else {
+        telefono.style.border = "1px solid black"
         return true;
     }
+}
+
+function validoemail() {
+    //debo validarlo por expresiones regulares preguntar el martes
+
+    if (email.value == "") {
+        console.log("pone el email");
+        email.placeholder = "Te falto el email";
+        email.focus();
+        email.style.border = "2px solid red"
+        return false;
+    } else {
+        email.style.border = "1px solid black"
+        return true;
     }
+}
 
 
 function validoradios() {
-  if(radios[0].checked || radios[1].checked){
-      console.log(radios.value)
-      console.log("Y");
-      return true;
-  }else{   
-    console.log("X")
-      return false;
-  }
+    if (radios[0].checked || radios[1].checked) {
+
+
+        return true;
+    } else {
+        console.log("X")
+
+        return false;
+    }
 
 }
-/*
+
 function crearElementos() {
-   saludo.innerHTML = "";
+    saludo.innerHTML = "";
+    console.log("aca esta el crear elementos");
     array?.forEach(aux => {
         let elemento = document.createElement("div");
-        elemento.className= "creado2";
-      if (radios[1].checked && aux.comentarios == "") {
-            elemento.innerHTML = "<p>" "Hola "+ aux.nombre + " gracias por contactarte con nosotros ""</p>"   
-
-        } else if (radios[0].checked && aux.comentarios != "") {
-            elemento.innerHTML = "<p>" "Hola " + aux.nombre + " Gracias por contactarte con nosotros y dejarnos un mensaje, tambien estamos contentos de que te guste nuestro sitio" "</p>"
+        elemento.className = "saludo2";
+        if (radios[1].checked && aux.comentarios == "" || radios[1].unchecked && radios[0].unchecked) {
+            elemento.innerHTML = "<p>" + "Hola " + aux.nombre + " gracias por contactarte con nosotros " + "</p>";
         } else if (radios[1].checked && aux.comentarios != "") {
-            elemento.innerHTML = "<p>" "Hola " + aux.nombre + " Gracias por contactarte con nosotros y dejarnos un mensaje ""</p>"
-        } else {
-            elemento.innerHTML = "<p>" + aux.nombre + " deja una sugerencia : " + aux.comentarios + "</p>"           
+            elemento.innerHTML = "<p>" + "Hola " + aux.nombre + " Gracias por contactarte con nosotros y dejarnos un mensaje " + "</p>"
+        } else if (radios[0].checked && aux.comentarios != "") {
+            elemento.innerHTML = "<p>" + "Hola " + aux.nombre + " Gracias por contactarte con nosotros y dejarnos un mensaje, tambien estamos contentos de que te guste nuestro sitio" + "</p>"
+        } else if (radios[0].checked && aux.comentarios == "") {
+            elemento.innerHTML = "<p>" + "Hola " + aux.nombre + " Gracias por contactarte con nosotros y nos alegra que te guste nuestra pagina " + "</p>"
+        }
+        else {
+            elemento.innerHTML = "<p>" + "Hola " + aux.nombre + "</p>";
         }
         saludo.appendChild(elemento)
     })
 }
-*/
