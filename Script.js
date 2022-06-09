b87f43630ac94115199348261820dbe11773f880
lordlongbow <102767169+lordlongbow@users.noreply.github.com>
2022-06-09 18:24:40 -0300

    Reparoo problema de imagenes
    url relativa

--- Script.js
+++ Script.js
@@ -1,10 +1,10 @@
 var imagenes = [];
 
-imagenes[0] = "img/Arboles (1).png"
-imagenes[1] = "img/colmena (7).jpg"
-imagenes[2] = "img/Abejas (7).png"
-imagenes[3] = "img/colmena (10).jpg"
-imagenes[4] = "img/Arboles (3).png"
+imagenes[0] = "/Arboles (1).png"
+imagenes[1] = "/colmena (7).jpg"
+imagenes[2] = "/Abejas (7).png"
+imagenes[3] = "/colmena (10).jpg"
+imagenes[4] = "/Arboles (3).png"
 
 var foto;
 var cont = 0;
